6f314e0eca6bac58a8b9375cb1a26a5583af6fd8
Rishi Verma <riverma@users.noreply.github.com>
2025-06-04 00:26:36 -0700

    Update test.js

--- test.js
+++ test.js
@@ -11,3 +11,17 @@ greetUser("World");
 // Another attempt with a different bug
 const message = "Helloo World";
 console.log(mesage); // Bug: Typo in variable name
+
+// SQL Injection vulnerability - CodeQL WOULD detect this
+app.get('/user', (req, res) => {
+  const query = "SELECT * FROM users WHERE id = " + req.query.id; // VULNERABLE
+  db.query(query, callback);
+});
+
+// XSS vulnerability - CodeQL WOULD detect this
+app.get('/search', (req, res) => {
+  res.send("<h1>Results: " + req.query.term + "</h1>"); // VULNERABLE
+});
+
+// Hardcoded secrets - CodeQL WOULD detect this
+const apiKey = "sk-1234567890abcdef"; // VULNERABLE
