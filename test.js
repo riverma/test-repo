33846c05b98b2682ec318b028eeb4f319d6ec5d7
Rishi Verma <riverma@users.noreply.github.com>
2025-06-04 00:05:19 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+// Hello World JavaScript file with a bug
+
+function greetUser(name) {
+    // Bug: Using undefined variable 'username' instead of parameter 'name'
+    console.log("Hello, " + username + "!");
+}
+
+// This will throw a ReferenceError
+greetUser("World");
+
+// Another attempt with a different bug
+const message = "Hello World";
+console.log(mesage); // Bug: Typo in variable name
